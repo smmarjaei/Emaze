fc517a2435fa4e5bc55b9f08aeeea5b28f83015d
smmarjaei <65842351+smmarjaei@users.noreply.github.com>
2020-08-11 21:39:20 -0400

    Update clock.js

--- countVersion 3/js/clock.js
+++ countVersion 3/js/clock.js
@@ -3,16 +3,16 @@ if (ClockType == "Extended") {
     var deadline;
     var CountDownDate = getParameterByName("date");
     console.log(CountDownDate);
-    if(CountDownDate==null){
-        deadline="nov 2, 2020 15:37:25"
-    }
-    else if(CountDownDate != "nov 2, 2020 15:37:25") {
+    if (CountDownDate == null) {
+        deadline = "nov 2, 2020 15:37:25"
+    } else if (CountDownDate != "nov 2, 2020 15:37:25") {
         deadline = CountDownDate;
     }
-    console.log(deadline);
+    // console.log(deadline);
     deadline = new Date(Date.parse(new Date(deadline)));
-    console.log("Deadline", deadline.getTime());
+    // console.log("Deadline", deadline.getTime());
 
+    var $font = $('.clock-col ');
 
     var bgColor = getParameterByName("bg-color");
     var fontColor = getParameterByName("font-color");
@@ -20,39 +20,41 @@ if (ClockType == "Extended") {
     $divClass = $('.clock-container');
     $divClass.css({
         'background-color': bgColor,
-        'color': fontColor,
         // 'Style': font-underline
     })
+    $font.css("color", fontColor)
+
 
     $divClass = $('.clock-timer');
     $divClass.css({
         'color': fontColor,
         // 'Style': font-underline
     })
-    
+
     var days = getParameterByName("days");
     var hours = getParameterByName("hours");
     var minutes = getParameterByName("minutes");
     var seconds = getParameterByName("seconds");
     var units = getParameterByName("units");
 
-    if(units=="false"){
+    if (units == "false") {
         console.log("In units");
+        $(".unit").css("display", "none");
+        // $("#d").css("display", "none");
+        // $("#h").css("display", "none");
+        // $("#m").css("display", "none");
+        // $("#s").css("display", "none");
+    }
+    if (days == "false") {
         $("#d").css("display", "none");
+    }
+    if (hours == "false") {
         $("#h").css("display", "none");
-        $("#m").css("display", "none");
-        $("#s").css("display", "none");
     }
-    if(days=="false"){
-        $("#d").css("display", "none");
-    }
-    if(hours=="false"){
-       $("#h").css("display", "none");
-   } 
-    if(minutes=="false"){
+    if (minutes == "false") {
         $("#m").css("display", "none");
     }
-    if(seconds=="false"){
+    if (seconds == "false") {
         $("#s").css("display", "none");
     }
 
@@ -62,7 +64,7 @@ if (ClockType == "Extended") {
     var x = setInterval(function() {
 
         var now = new Date().getTime();
-        console.log("now", now);
+        // console.log("now", now);
         var t = deadline - now;
         var days = Math.floor(t / (1000 * 60 * 60 * 24));
         var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -82,7 +84,7 @@ if (ClockType == "Extended") {
         }
     }, 1000);
 }
- //    document.addEventListener('DOMContentLoaded', () =>
+//    document.addEventListener('DOMContentLoaded', () =>
 //    requestAnimationFrame(updateTime)
 //  )
 
@@ -93,4 +95,3 @@ if (ClockType == "Extended") {
 //         // document.documentElement.style.setProperty('--timer-seconds', "'" + moment().format("ss") + "'");
 //         // requestAnimationFrame(updateTime);
 //  }
-
