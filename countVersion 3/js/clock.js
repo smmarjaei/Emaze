8da691b6e5f63a8e853f03f101f9387dc348f42b
smmarjaei <65842351+smmarjaei@users.noreply.github.com>
2020-08-11 22:03:07 -0400

    Update clock.js

--- countVersion 3/js/clock.js
+++ countVersion 3/js/clock.js
@@ -1,16 +1,5 @@
 var ClockType = getParameterByName("clock-type");
 if (ClockType == "Extended") {
-    var deadline;
-    var CountDownDate = getParameterByName("date");
-    console.log(CountDownDate);
-    if (CountDownDate == null) {
-        deadline = "nov 2, 2020 15:37:25"
-    } else if (CountDownDate != "nov 2, 2020 15:37:25") {
-        deadline = CountDownDate;
-    }
-    // console.log(deadline);
-    deadline = new Date(Date.parse(new Date(deadline)));
-    // console.log("Deadline", deadline.getTime());
 
     var $font = $('.clock-col ');
 
@@ -59,39 +48,59 @@ if (ClockType == "Extended") {
     }
 
 
-    // var deadline = new Date("nov 1, 2020 15:37:25").getTime(); 
+    var deadline;
+    var CountDownDate = getParameterByName("date");
+    // console.log(CountDownDate);
+    if (CountDownDate == null) {
+        document.getElementById("day").innerHTML = "00";
+        document.getElementById("hour").innerHTML = "00";
+        document.getElementById("minute").innerHTML = "00";
+        document.getElementById("second").innerHTML = "00";
 
-    var x = setInterval(function() {
+    } else if (CountDownDate != "nov 2, 2020 15:37:25") {
+        deadline = CountDownDate;
 
-        var now = new Date().getTime();
-        // console.log("now", now);
-        var t = deadline - now;
-        var days = Math.floor(t / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((t % (1000 * 60)) / 1000);
-        document.getElementById("day").innerHTML = days;
-        document.getElementById("hour").innerHTML = hours;
-        document.getElementById("minute").innerHTML = minutes;
-        document.getElementById("second").innerHTML = seconds;
-        if (t < 0) {
-            clearInterval(x);
-            document.getElementById("timeRemaining").innerHTML = "TIME UP";
-            document.getElementById("day").innerHTML = '0';
-            document.getElementById("hour").innerHTML = '0';
-            document.getElementById("minute").innerHTML = '0';
-            document.getElementById("second").innerHTML = '0';
-        }
-    }, 1000);
+        deadline = new Date(Date.parse(new Date(deadline)));
+
+        var x = setInterval(function() {
+
+            var now = new Date().getTime();
+            // console.log("now", now);
+            var t = deadline - now;
+
+
+
+            var days = Math.floor(t / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((t % (1000 * 60)) / 1000);
+
+            if (days < 1) {
+                days = '00';
+            }
+            if (hours < 1) {
+                hours = '00';
+            }
+            if (minutes < 1) {
+                minutes = '00';
+            }
+
+            document.getElementById("day").innerHTML = days;
+            document.getElementById("hour").innerHTML = hours;
+            document.getElementById("minute").innerHTML = minutes;
+            document.getElementById("second").innerHTML = seconds;
+
+            if (t < 0) {
+                clearInterval(x);
+                // document.getElementById("timeRemaining").innerHTML = "TIME UP";
+                document.getElementById("day").innerHTML = '00';
+                document.getElementById("hour").innerHTML = '00';
+                document.getElementById("minute").innerHTML = '00';
+                document.getElementById("second").innerHTML = '00';
+            }
+
+
+        }, 1000);
+    }
+
 }
-//    document.addEventListener('DOMContentLoaded', () =>
-//    requestAnimationFrame(updateTime)
-//  )
-
-//  function updateTime() {
-//         // document.documentElement.style.setProperty('--timer-day', "'" + moment().format("dd") + "'");
-//         // document.documentElement.style.setProperty('--timer-hours', "'" + moment().format("k") + "'");
-//         // document.documentElement.style.setProperty('--timer-minutes', "'" + moment().format("mm") + "'");
-//         // document.documentElement.style.setProperty('--timer-seconds', "'" + moment().format("ss") + "'");
-//         // requestAnimationFrame(updateTime);
-//  }
