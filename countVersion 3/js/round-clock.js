68783961f6fad27e2264e265cb4b48a0639a9bec
smmarjaei <65842351+smmarjaei@users.noreply.github.com>
2020-08-12 00:21:46 -0400

    Update round-clock.js

--- countVersion 3/js/round-clock.js
+++ countVersion 3/js/round-clock.js
@@ -60,7 +60,7 @@ if (ClockType == "Clocks") {
             // Defines how the ending of the bar line looks like. Possible values are: butt, round and square.
             lineCap: 'round',
             // Width of the bar line in px.
-            lineWidth: 12,
+            lineWidth: 18,
             // Size of the pie chart in px. It will always be a square.
             size: 200,
             // Time in milliseconds for a eased animation of the bar growing, or false to deactivate.
