36c178e1a917a4e0546a0e4ea7707d03e1e8c044
smmarjaei <65842351+smmarjaei@users.noreply.github.com>
2020-08-12 13:48:56 -0400

    Added

---
+++ card-hover-interactions/dist/app.js
@@ -0,0 +1,56 @@
+var $Heading1 = $('#heading_1');
+var $Para1 = $('#body_1');
+var $Button1 = $('#button_1');
+
+var Title1 = getParameterByName("title_1");
+var Info1 = getParameterByName("info_1");
+var Btn1 = getParameterByName("btn_1");
+var Img1 = getParameterByName("img_1");
+
+
+var $Heading2 = $('#heading_2');
+var $Para2 = $('#body_2');
+var $Button2 = $('#button_2');
+
+var Title2 = getParameterByName("title_2");
+var Info2 = getParameterByName("info_2");
+var Btn2 = getParameterByName("btn_2");
+var Img2 = getParameterByName("img_2");
+
+
+var $Heading3 = $('#heading_3');
+var $Para3 = $('#body_3');
+var $Button3 = $('#button_3');
+
+var Title3 = getParameterByName("title_3");
+var Info3 = getParameterByName("info_3");
+var Btn3 = getParameterByName("btn_3");
+var Img3 = getParameterByName("img_3");
+
+
+var $Heading4 = $('#heading_4');
+var $Para4 = $('#body_4');
+var $Button4 = $('#button_4');
+
+var Title4 = getParameterByName("title_4");
+var Info4 = getParameterByName("info_4");
+var Btn4 = getParameterByName("btn_4");
+var Img4 = getParameterByName("img_4");
+
+
+
+$Heading1.css("title_1", Title1);
+$Para1.css("info_1", Info1);
+$Button1.css("btn_1", Btn1);
+
+$Heading2.css("title_2", Title2);
+$Para2.css("info_2", Info2);
+$Button2.css("btn_2", Btn2);
+
+$Heading3.css("title_3", Title3);
+$Para3.css("info_3", Info3);
+$Button3.css("btn_3", Btn3);
+
+$Heading4.css("title_4", Title4);
+$Para4.css("info_4", Info4);
+$Button4.css("btn_4", Btn4);
