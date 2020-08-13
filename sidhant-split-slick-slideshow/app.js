34de0baf6581896e2df670ce9344a7734065bf10
Sidhantbahl <sibahl@ucsc.edu>
2020-08-13 12:30:59 +0530

    Added

--- sidhant-split-slick-slideshow/app.js
+++ sidhant-split-slick-slideshow/app.js
@@ -2,21 +2,29 @@
 var $Title1 = $('#slide_1');
 var $Img1 = $('#img_1');
 
-var title_1 = getParameterByName("title_1");
-var Img1 = getParameterByName("img_1");
+var head1 = getParameterByName("title_1");
+var Img1 = getParameterByName("imgage_1");
 
-$Title1.html(title_1);
-$Img1.html(`<img src="${Img1}"/>`);
-// $("#img_1").append(`<img src="${Img1}" />`);
+$Title1.html(head1);
+// $Img1.html(`<img src="${Img1}"/>`);
+// // $("#img_1").append(`<img src="${Img1}" />`);
+$divclass=$('#img_1');
+<style>
+$divclass.html({
+    background-image: url(${Img1});
+})
+</style>
+
+
 
 //IMG 2
 var $Title2 = $('#slide_2');
 var $Img2 = $('#img_2');
 
-var title_2 = getParameterByName("title_2");
-var Img2 = getParameterByName("img_2");
+var head2 = getParameterByName("title_2");
+var Img2 = getParameterByName("imgage_2");
 
-$Title2.html(title_2);
+$Title2.html(head2);
 $Img2.html(`<img src="${Img2}"/>`);
 
 
@@ -24,10 +32,10 @@ $Img2.html(`<img src="${Img2}"/>`);
 var $Title3 = $('#slide_3');
 var $Img3 = $('#img_3');
 
-var title_3 = getParameterByName("title_3");
-var Img3 = getParameterByName("img_3");
+var head3 = getParameterByName("title_3");
+var Img3 = getParameterByName("imgage_3");
 
-$Title3.html(title_3);
+$Title3.html(head3);
 $Img3.html(`<img src="${Img3}"/>`);
 
 
@@ -35,8 +43,8 @@ $Img3.html(`<img src="${Img3}"/>`);
 var $Title4 = $('#slide_4');
 var $Img4 = $('#img_4');
 
-var title_4 = getParameterByName("title_4");
-var Img4 = getParameterByName("img_4");
+var head4 = getParameterByName("title_4");
+var Img4 = getParameterByName("imgage_4");
 
-$Title4.html(title_4);
+$Title4.html(head4);
 $Img4.html(`<img src="${Img4}"/>`);
